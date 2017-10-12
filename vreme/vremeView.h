948bb5dd719e75bd57681012fb1fac80f70905d1
Desislav Hristov <hristov.desislav@gmail.com>
2017-10-13 01:11:21 +0300

    Initial Commit

---
+++ vreme/vremeView.h
@@ -0,0 +1,13 @@
+//
+//  vremeView.h
+//  vreme
+//
+//  Created by Desislav Hristov on 13/10/2017.
+//  Copyright © 2017 Desislav Hristov. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface vremeView : ScreenSaverView
+
+@end
